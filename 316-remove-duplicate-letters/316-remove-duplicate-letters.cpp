a2956c701f54a81d49d5a394d7e07ff8a1b2c9e6
Daksh Paleria <dpaleria@gmail.com>
2021-10-20 16:31:18 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (55.81%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        stack<char> st;
+        vector<int> alpha(26);
+        vector<int> vis(26);
+        for(int i=0; i<s.length(); i++)
+        {
+            alpha[s[i]-'a']=i;
+        }
+        for(int i=0; i<s.length(); i++)
+        {
+            if(vis[s[i]-'a'])
+            {
+                continue;
+            }
+            while(!st.empty() && st.top()>s[i] && i<alpha[st.top()-'a'])
+            {
+                vis[st.top()-'a']=0;
+                st.pop();
+            }
+            st.push(s[i]);
+            vis[s[i]-'a']=1;
+        }
+        string res;
+        while(!st.empty())
+        {
+            res+=st.top();
+            st.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
