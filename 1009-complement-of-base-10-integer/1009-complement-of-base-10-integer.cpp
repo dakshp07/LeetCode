afc326bc672d34804ecafb29688ef5ad15b4eb3f
Daksh Paleria <dpaleria@gmail.com>
2022-01-04 15:07:07 +0530

    Time: 0 ms (100.00%), Space: 6 MB (31.93%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0){
+            return 1;
+        }
+        int len=0,num=n;
+        while(num!=0){
+            len++;
+            num/=2;
+        }
+        long long int m=pow(2,len)-1;
+        return m-n;
+    }
+};
