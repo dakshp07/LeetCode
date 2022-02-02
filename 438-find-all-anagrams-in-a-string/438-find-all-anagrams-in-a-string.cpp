1f6c0e857bc29eaf982a54adc02c5e9733c2695e
Daksh Paleria <dpaleria@gmail.com>
2022-02-02 10:22:32 +0530

    Time: 17 ms (63.57%), Space: 8.7 MB (42.26%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> char_count(26);
+        vector<int> freq(26, 0);
         for(int i=0; i<p.length(); i++)
         {
-            char_count[p[i]-'a']++;
+            freq[p[i]-'a']++;
         }
-        int left=0;
-        int right=0;
-        int count=p.length();
         vector<int> res;
+        int left=0, right=0, count=p.length();
         while(right<s.length())
         {
-            if(char_count[s[right++]-'a']-->=1)
+            if(freq[s[right++]-'a']-->=1)
             {
+                // freq[s[right]-'a']--; The commented lines give TLE
+                // right++;
                 count--;
             }
             if(count==0)
             {
                 res.push_back(left);
             }
-            if(right-left==p.length() && char_count[s[left++]-'a']++>=0)
+            if(right-left==p.length() && freq[s[left++]-'a']++>=0)
             {
+                // freq[s[left]-'a']++; The commented lines give TLE
+                // left++;
                 count++;
             }
         }
