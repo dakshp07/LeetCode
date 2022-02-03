4992dfa4cc8db2c80144aea298c7632a2f981485
Daksh Paleria <dpaleria@gmail.com>
2022-02-03 09:41:28 +0530

    Time: 127 ms (24.15%), Space: 30.9 MB (80.33%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
-        int ans=0;
+        // Brute: 89ms and 49.18% of rest submission
+        // int ans=0;
+        // for(int i=0; i<items.size(); i++)
+        // {
+        //     if((ruleKey=="type" && ruleValue==items[i][0]) || (ruleKey=="color" && ruleValue==items[i][1]) || (ruleKey=="name" && ruleValue==items[i][2]))
+        //     {
+        //         ans++;
+        //     }
+        // }
+        // return ans;
+        
+        // Optimised
+        int j=0, ans=0;
+        if(ruleKey=="type") j=0;
+        if(ruleKey=="color") j++;
+        if(ruleKey=="name") j+=2;
         for(int i=0; i<items.size(); i++)
         {
-            if((ruleKey=="type" && ruleValue==items[i][0]) || (ruleKey=="color" && ruleValue==items[i][1]) || (ruleKey=="name" && ruleValue==items[i][2]))
+            if(items[i][j]==ruleValue)
             {
                 ans++;
             }
