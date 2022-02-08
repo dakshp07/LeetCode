09706a2231ee7a97fbad213613e45e57370e7367
Daksh Paleria <dpaleria@gmail.com>
2022-02-08 09:29:28 +0530

    Time: 8 ms (8.74%), Space: 6 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
-    int addDigits(int n) {
-        int sum=0;
-        while(n)
+    int addDigits(int num) {
+        // string str=to_string(num);
+        // string temp=str;
+        // int sum=0;
+        // while(temp.size()!=1)
+        // {
+        //     for(int i=0; i<temp.size(); i++)
+        //     {
+        //         sum+=(str[i]-'0');
+        //     }
+        //     temp+=to_string(sum);
+        // }
+        // return stoi(temp);
+        string str=to_string(num);
+        while(str.size()!=1)
         {
-            sum+=n%10;
-            n/=10;
+            int sum=0;
+            for(int i=0; i<str.size(); i++)
+            {
+                sum+=(str[i]-'0');
+            }
+            str=to_string(sum);
         }
-        if(sum/10==0)return sum;
-        return addDigits(sum);
+        return stoi(str);
     }
 };
