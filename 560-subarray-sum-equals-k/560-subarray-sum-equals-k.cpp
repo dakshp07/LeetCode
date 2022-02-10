41f401e83a7c36d78a823edc4d100aada283227d
Daksh Paleria <dpaleria@gmail.com>
2022-02-10 09:38:03 +0530

    Time: 191 ms (11.17%), Space: 38.3 MB (42.50%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         map<int, int> mp;
         int count=0, sum=0;
-        mp[0]=1;
+        mp[0]=1; // storing the count of times when i saw a sum
         for(int i=0; i<n; i++)
         {
             sum+=nums[i];
