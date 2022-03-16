cb16251396d2bc192ec7c2e344907aae1aa952f9
Daksh Paleria <dpaleria@gmail.com>
2022-03-16 11:49:07 +0530

    Time: 25 ms (71.13%), Space: 11.7 MB (41.69%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,35 +2,26 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         stack<int> st;
-        for(int i=0; i<s.size(); i++)
-        {
-            // agar open para hain toh push
-            if(s[i]=='(')
-            {
+        int i = 0;
+        for(i=0;i<s.length();i++){
+            if(s[i] == '(' ){
                 st.push(i);
-            }
-            // agar closed hain or stack empty nhi toh stack ke top pe open hoga uska pop kardo
-            else if(s[i]==')' && !st.empty())
-            {
+            }   
+            else if(s[i]==')' && !st.empty()){
                 st.pop();
             }
-            // agar stack khali hain matlab ye para ka koi pair nhi so exclude
-            else if(s[i]==')' && st.empty())
-            {
-                s[i]='#';
+            else if(s[i] == ')' && st.empty()){
+                s[i]='@';
             }
         }
-        // abhi agar stack khai nhi huyi matlab kuch para abhi bhi aise hain jinka pair nhi
-        while(!st.empty())
-        {
-            s[st.top()]='#';
+        while(!st.empty()){
+            s[st.top()] = '@';
             st.pop();
         }
         string res;
-        // string banao
         for(int i=0; i<s.length(); i++)
         {
-            if(s[i]!='#')
+            if(s[i]!='@')
             {
                 res.push_back(s[i]);
             }
