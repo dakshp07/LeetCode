f271e2caa0b57c0989de35352e03fe74e0d0f8f6
Daksh Paleria <dpaleria@gmail.com>
2022-04-20 10:56:00 +0530

    Update 557-reverse-words-in-a-string-iii.cpp

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     string reverseWords(string s) {
         vector<string> words;
+        // stringstream se words without space store karo
         stringstream st(s);
         string word;
         while(st>>word)
         {
+            // vector main store karo words
             words.push_back(word);
         }
         string ans="";
         for(int i=0; i<words.size(); i++)
         {
+            // individual words ko reverse karo
             string temp=words[i];
             reverse(temp.begin(), temp.end());
+            // ans string main append karo
             ans+=temp;
+            // space lagate jaao
             ans+=" ";
         }
+        // last space pop back
         ans.pop_back();
         return ans;
     }
