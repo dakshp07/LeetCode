cda1bb8ec8594c26d1fb04326336bff242f7828d
Daksh Paleria <dpaleria@gmail.com>
2022-05-21 12:15:21 +0530

    Time: 5 ms (45.66%), Space: 10.2 MB (72.53%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -2,7 +2,32 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         // simple sol but n*logn
-        sort(nums.begin(), nums.end());
-        return nums[0];
+        // sort(nums.begin(), nums.end());
+        // return nums[0];
+        
+        
+        // now lets see logn
+        // binary lagake dekhte kyuki thats an logn solution
+        int left=0;
+        int right=nums.size()-1;
+        while(left<right)
+        {
+            int mid=(left+right)/2;
+            // agar mid-1 bada hogaya mid se matlab mid hi sabse chota
+            if(mid>0 && nums[mid]<nums[mid-1])
+            {
+                return nums[mid];
+            }
+            // agar left chota hain mid se and right bhi chota hain mid se toh mujhe 
+            else if(nums[left]<=nums[mid] && nums[mid]>nums[right])
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid-1;
+            }
+        }
+        return nums[left];
     }
 };
