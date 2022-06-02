2064f8f0858864cd35f4fddd532d3a36c9413366
Daksh Paleria <dpaleria@gmail.com>
2022-06-02 10:22:58 +0530

    Time: 11 ms (38.13%), Space: 10.7 MB (16.11%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     string simplifyPath(string path) {
+        // stack ka standard question
         stack<string> st;
         for(int i=0; i<path.length(); i++)
         {
+            // end main laga denge /
+            if(path[i]=='/') continue;
+            // char ko temp main daalo jab taak / nhi aata
             string temp="";
-            if(path[i]=='/')
-            {
-                continue;
-            }
             while(i<path.length() && path[i]!='/')
             {
                 temp+=path[i];
                 i++;
             }
-            if(temp==".")
-            {
-                continue;
-            }
+            // agar 1 . aaya toh continue
+            if(temp==".") continue;
+            // agar 2 .. aagaye toh pop
             if(temp=="..")
             {
                 if(!st.empty())
@@ -25,21 +24,18 @@ public:
                     st.pop();
                 }
             }
-            else
-            {
-                st.push(temp);
-            }
+            // baaki sab condition main push
+            else st.push(temp);
         }
+        // ab stack khali karo
         string res="";
         while(!st.empty())
         {
             res="/"+st.top()+res;
             st.pop();
         }
-        if(res.length()==0)
-        {
-            return "/";
-        }
+        // agar kuch nhi baacha
+        if(res.length()==0) return "/";
         return res;
     }
 };
