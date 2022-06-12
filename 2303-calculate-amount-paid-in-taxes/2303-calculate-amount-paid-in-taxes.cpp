4feb984e3fef0a552969f45fb807b042c12d5af2
Daksh Paleria <dpaleria@gmail.com>
2022-06-12 12:32:34 +0530

    Time: 25 ms (75.00%), Space: 13.3 MB (50.00%) - LeetHub

---
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double calculateTax(vector<vector<int>>& brackets, int income) {
+        if(income==0) return 0.0;
+        double ans=0;
+        double prev=0;
+        for(int i=0; i<brackets.size(); i++)
+        {
+            int curr=min(brackets[i][0], income);
+            ans+=((curr-prev)*brackets[i][1]/100.0);
+            if(brackets[i][0]>income) break;
+            prev=brackets[i][0];
+        }
+        return ans;
+    }
+};
