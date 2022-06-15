887186eac90cdec05f950edf3b080c61371a6c52
Daksh Paleria <dpaleria@gmail.com>
2022-06-15 17:45:48 +0530

    Time: 44 ms (5.28%), Space: 13.4 MB (10.97%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        //simple kaam phele
-        // store row and col of ele ie is 0
-        set<int> rows, cols; // set isliye taaki .fnnd use kar sake
+        // set use karte hain
+        // will store the index of row, col where ele is 0
+        set<int> rows, cols;
         for(int i=0; i<matrix.size(); i++)
         {
             for(int j=0; j<matrix[i].size(); j++)
             {
-                if(matrix[i][j]==0)
+                if(matrix[i][j]==0) // if an ele is 0
                 {
-                    rows.insert(i);
-                    cols.insert(j);
+                    rows.insert(i); // row ke index ko daalo rows main
+                    cols.insert(j); // col ke index ko daalo cols main
                 }
             }
         }
-        // 1 more iteration, check with sets and make that row and col element as zero
+        // now traverse again
+        // look for the row, col in both the sets respectively
+        // if the row or col is in set then make that ele as 0
         for(int i=0; i<matrix.size(); i++)
         {
             for(int j=0; j<matrix[i].size(); j++)
             {
-                if(rows.find(i)!=rows.end() || cols.find(j)!=cols.end())
+                if(rows.find(i)!=rows.end() || cols.find(j)!=cols.end()) // if that index is part of any of the two sets
                 {
-                    matrix[i][j]=0;
+                    matrix[i][j]=0; // we make that ele as 0
                 }
             }
         }
