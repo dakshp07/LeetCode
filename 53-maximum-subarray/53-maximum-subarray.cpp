a0cbcadaf554d945e5d37d1a4588d7547e1aabec
Daksh Paleria <dpaleria@gmail.com>
2022-06-15 21:30:16 +0530

    Time: 148 ms (53.65%), Space: 67.7 MB (90.66%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         // kadane lagao
-        int curr=nums[0], sum=nums[0];
+        int curr_sum=nums[0], max_sum=nums[0];
         for(int i=1; i<nums.size(); i++)
         {
             // yaa toh curr+nums[i] ko lenge yaa phir sirf nums[i] ko
             // max lenge
-            curr=max(curr+nums[i], nums[i]);
+            curr_sum=max(curr_sum+nums[i], nums[i]);
             // sum main bhi curr, sum ka max lenge
-            sum=max(sum, curr);
+            max_sum=max(max_sum, curr_sum);
         }
-        return sum;
+        return max_sum;
     }
 };
