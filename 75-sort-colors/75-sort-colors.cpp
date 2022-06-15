ac099672b8b950b8181c96a062a2ea39daf3c49e
Daksh Paleria <dpaleria@gmail.com>
2022-06-15 21:41:00 +0530

    Time: 3 ms (60.58%), Space: 8.2 MB (90.49%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        int length=nums.size();
-        int left=0;
-        int right=length-1;
-        int curr=0;
-        while(curr<=right && left<right)
+    void sortColors(vector<int>& arr) {
+        // one could be use sort
+        // other could be store the numbers in freq array
+        // third is to use dutch national flag algo, more on this algo:
+        // we have three pointers, low=0, mid=0, high=size-1
+        // assume:
+        // ele in o to low-1 are 0s
+        // ele in mid+1 to high are 2s
+        // rest ele b/w low to mid are 1s
+        // how to do:
+        // if(mid==0) swap(mid, low) mid++ low++
+        // mid==1 mid++
+        // mid==2 swap(mid, high) high--
+        int low=0, mid=0, high=arr.size()-1;
+        while(mid<=high)
         {
-            if(nums[curr]==0)
+            if(arr[mid]==0)
             {
-                swap(nums[left], nums[curr]);
-                left++;
-                curr++;
+                swap(arr[mid], arr[low]);
+                mid++;
+                low++;
             }
-            else if(nums[curr]==2)
+            else if(arr[mid]==1)
             {
-                swap(nums[right], nums[curr]);
-                right--;
+                mid++;
             }
-            else
+            else if(arr[mid]==2)
             {
-                curr++;
+                swap(arr[mid], arr[high]);
+                high--;
             }
         }
     }
