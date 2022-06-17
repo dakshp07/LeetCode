099cb681aff21674e189be119504b983a33d5a10
Daksh Paleria <dpaleria@gmail.com>
2022-06-17 11:33:45 +0530

    Time: 69 ms (29.83%), Space: 18.9 MB (84.79%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> mergedInterval;
-        
-        if(intervals.size()==0)
-            return mergedInterval;
-        
-        sort(intervals.begin(),intervals.end());
-        
-        vector<int> tempInterval = intervals[0];
-        
-        for(auto interval : intervals){
-            
-            if(interval[0] <= tempInterval[1] )
-                tempInterval[1] = max(interval[1],tempInterval[1]);
-            
-            else { mergedInterval.push_back(tempInterval); tempInterval=interval; }
-            
+        // we have to check the end with the start of next interval
+        sort(intervals.begin(), intervals.end()); // sort to make sure that they are in order
+        vector<vector<int>> res; // our last array
+        int prev_start=intervals[0][0]; // start
+        int prev_end=intervals[0][1]; // end
+        for(int i=1; i<intervals.size(); i++)
+        {
+            if(intervals[i][0]<=prev_end) // agar next interval ka start is less than or equal to prev ka end
+            {
+                prev_end=max(prev_end, intervals[i][1]); // toh update with max
+            }
+            else // agar end bada hain next ke start se matalb we found the overlap, update the prev start and end
+            {
+                res.push_back({prev_start, prev_end}); // push in res
+                prev_start=intervals[i][0]; // update
+                prev_end=intervals[i][1];
+            }
         }
-        
-        mergedInterval.push_back(tempInterval);
-        return mergedInterval;
+        res.push_back({prev_start, prev_end}); // whatever is remaining will push that too
+        return res;
     }
 };
