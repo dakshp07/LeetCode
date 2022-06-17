4fa63da9545180f3937c7357ba72657c000f4a61
Daksh Paleria <dpaleria@gmail.com>
2022-06-17 11:49:16 +0530

    Time: 4 ms (51.25%), Space: 9.1 MB (71.62%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // 2 ptr se ez
-        // current pos ka track ke liye curr
-        int curr=m+n-1;
-        // last main point kar rahe 2 ptr
-        m--;
-        n--;
-        // 2 ptr lagao
+        // 2 ptrs laga sakte
+        // ek ptr hoga nums1 pe or ek nums2 pe
+        int curr=m+n-1; // ye current pe point karega, joh peeche se hoga chalu
+        // peeche se chalu
+        m--, n--;
         while(m>=0 && n>=0)
         {
-            // agar nums1 > nums2
+            // agar nums1 bada hua matlab mujhe peeche daalna usko
             if(nums1[m]>nums2[n])
             {
-                // usko curr pe daalo
-                // curr-- karo and m--
-                nums1[curr]=nums1[m];
-                curr--;
-                m--;
+                nums1[curr]=nums1[m]; // peeche daalna matlab curr pr daalna
+                curr--; // curr ko aage karo
+                m--; // m ko bhi
             }
-            // baaki cases
+            // agar nums2 bada hua toh woh peeche aayega ie ab nums2 curr pe jaayega
             else
             {
-                // curr pe daalo 
-                // curr-- and n--
-                nums1[curr]=nums2[n];
-                curr--;
-                n--;
+                nums1[curr]=nums2[n]; // daaldo curr pe
+                curr--; // curr ko aage karo
+                n--; // n ko bhi aage karo
             }
         }
-        // maan lo n wali arr badi hain and kuch ele rhe gaye usmain
-        // note: first waali array choti nhi hogi kyuki woh already m+n size ki hain
-        while(n>=0)
+        // aisa ho sakta ki n waali array ie nums2 main element baaki rahenge kyuki agar woh badi huyi toh
+        // nums1 main kabhi bhi ele nhi bachenge kyuki woh already m+n size hain
+        while(n>=0) // jitne baaki sab peeche jaayenge
         {
-            // woh sab curr pe jaayenge
-            // curr-- and n--;
-            nums1[curr]=nums2[n];
-            curr--;
-            n--;
+            nums1[curr]=nums2[n]; // daaldo curr pe
+            curr--; // curr ko aage karo
+            n--; // n ko bhi aage karo
         }
     }
 };
