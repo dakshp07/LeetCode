952898a8141a9c98f85a51b69ba18763b8db73d4
Daksh Paleria <dpaleria@gmail.com>
2022-06-17 11:59:58 +0530

    Time: 177 ms (50.37%), Space: 61.4 MB (28.20%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int length=nums.size();
-        int res=0;
-        for(int i=0; i<length-1; i++)
-        {
-            if(nums[i]==nums[i+1])
-            {
-                res=nums[i];
-            }
+        // kaafi methods hain
+        // 1) sort karke next waale check karo (violates)
+        // 2) set main seen store karo (violates)
+        // 3) map main freq lagao (violates)
+        // can't do any of the above kyuki that violates
+        
+        // tortoise-hare lagao
+        int slow=nums[0];
+        int fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
         }
-        return res;
+        while(slow!=fast);
+        fast=nums[0];
+        while(slow!=fast)
+        {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
     }
 };
