416bd5cb259c24f619d31d7ef684c322523c7013
Daksh Paleria <dpaleria@gmail.com>
2022-06-18 10:58:09 +0530

    Time: 38 ms (19.39%), Space: 19.4 MB (92.23%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // joh element n/2 se zyaada baari aaya hoga woh sort karte time ek taraf aajayega
-        // 22223 sort kiya hua hain toh 2 aagaya n/2 pos pe
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()/2];
+        // multiple ways:
+        // pick one element and check if it occurs n/2 times, n^2
+        // sort (violates) nlogn
+        // use freq array or map, space complexity
+        // moore voting algo lagao
+        // traverse the arr, keep cnt and ele
+        // if cnt==0 update ele with curr ele
+        // else if ele==arr[i] -> cnt++ or else cnt--;
+        // return ele
+        int ele=0, cnt=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(cnt==0) ele=nums[i];
+            if(ele==nums[i]) cnt++;
+            else cnt--;
+        }
+        return ele;
     }
 };
