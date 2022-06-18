b8265be158cfda76136b9832f112798c0e7aabf1
Daksh Paleria <dpaleria@gmail.com>
2022-06-18 11:11:02 +0530

    Time: 11 ms (93.94%), Space: 15.9 MB (51.85%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> umap;
-        vector<int> res;
-        int length=nums.size();
-        for(int i=0; i<length; i++)
+        // multiple ways:
+        // check for every ele, n^2
+        // sort karlo, nlogn
+        // use map or freq array, space complexity
+        // boyer moore lagega but todha obs ke saath
+        // at max there could be two elemts coming more than n/3 times
+        // toh normal boyer moore lagao or ye 2 ele pe check karte jaao
+        int nums1=-1, nums2=-1;
+        int c1=0, c2=0;
+        for(int i=0; i<nums.size(); i++)
         {
-            umap[nums[i]]++;
+            if(nums[i]==nums1) c1++;
+            else if(nums[i]==nums2) c2++;
+            else if(c1==0) c1=1, nums1=nums[i];
+            else if(c2==0) c2=1, nums2=nums[i];
+            else c1--, c2--;
         }
-        for(auto x: umap)
+        vector<int> ans; // since we have to return an arr and we have our two possibility in nums1, nums2
+        c1=0, c2=0;
+        for(int i=0; i<nums.size(); i++)
         {
-            if(x.second>length/3)
-            {
-                res.push_back(x.first);
-            }
+            if(nums[i]==nums1) c1++; // check count of each
+            else if(nums[i]==nums2) c2++;
         }
-        return res;
+        if(c1>nums.size()/3) // if > size/3 add it
+        {
+            ans.push_back(nums1);
+        }
+        if(c2>nums.size()/3)
+        {
+            ans.push_back(nums2);
+        }
+        return ans;
     }
 };
