ce63b469a1d3b0a1307e68d008632958a9191f24
Daksh Paleria <dpaleria@gmail.com>
2022-06-19 12:17:03 +0530

    Time: 1217 ms (7.22%), Space: 49.9 MB (27.84%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,54 +1,28 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        // sort kar sakte hain but that will be O(nlogn) time
-        // sort karke dekhte hain
-        // if(nums.size()==0) return 0;
-        // sort(nums.begin(), nums.end());
-        // int mxStreak=1, currStreak=1;
-        // for(int i=1; i<nums.size(); i++)
-        // {
-        //     if(nums[i]==nums[i-1]+1)
-        //     {
-        //         currStreak++;
-        //     }
-        //     else if(nums[i]==nums[i-1])
-        //     {
-        //         continue;
-        //     }
-        //     else
-        //     {
-        //         mxStreak=max(mxStreak, currStreak);
-        //         currStreak=1;
-        //     }
-        // }
-        // return max(mxStreak, currStreak);
-        
-        // now doing it in O(n) time
-        // we will use set to keep track of elements
+        // ek toh hain ki sort karlo or current streak, max maintain karo
+        // sort is o(nlogn)
+        // but we have to do in o(n)
+        // set main daalo sabko and check if nums[i]-1 is present or not
         unordered_set<int> st;
-        // saare ele add karo in set
+        int longStreak=0;
+        for(int i=0; i<nums.size(); i++) st.insert(nums[i]);
         for(int i=0; i<nums.size(); i++)
         {
-            st.insert(nums[i]);
-        }
-        // check karo if ele ka ele-1 hain kya set main
-        // agar haa matlab ye ele nhi hain humaare seq ka start
-        // agar naa matlab idhar se seq start hoga
-        int res=0;
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(st.count(nums[i]-1)==0)
+            if(!st.count(nums[i]-1)) // if set has nums[i]-1 ele
             {
-                int cnt=1;
-                // ab baaki ka seq check karo +1 karte karte
-                while(st.count(nums[i]+cnt)>0)
+                int currNum=nums[i]; // we set curr value as that nums
+                int currStreak=1; // and start streak here
+                while(st.count(currNum+1)) // we try to find more nums+1
                 {
-                    cnt++;
+                    currNum+=1; // and set the curr value to curr
+                    currStreak+=1; // increment curr streak by one
                 }
-                res=max(res, cnt);
+                longStreak=max(currStreak, longStreak); // pick the longest as ans
             }
         }
-        return res;
+        return longStreak;
     }
 };
+
