9216610f81f39cf1c548877107ec2dc1d13e0437
Daksh Paleria <dpaleria@gmail.com>
2022-06-19 12:57:57 +0530

    Time: 15 ms (73.76%), Space: 8.2 MB (81.47%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        // using a freq array to store the indexes
+        vector<int> freq(256, -1);
+        int left=0, right=0, len=0; // keep a pointer left, right starting at 0
+        while(right<s.size())
+        {
+            if(freq[s[right]]!=-1) // check the freq of s[right]
+            {
+                left=max(freq[s[right]]+1, left); // update it with the most recent index
+            }
+            freq[s[right]]=right; // if it doesnt exist we make it
+            len=max(len, right-left+1); // update the len
+            right++; // update right
+        }
+        return len;
+    }
+};
+
+/*
+another approach:
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
         // sliding window use karenge  and use a map to keep the track of visited elements
         unordered_map<int, int> mp;
         int res=0; // length of window
@@ -25,3 +47,4 @@ public:
         return res;
     }
 };
+*/
