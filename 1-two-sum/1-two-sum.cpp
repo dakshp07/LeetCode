03de247dbaef4c192e30d10082f549d7fb6ca720
Daksh Paleria <dpaleria@gmail.com>
2022-06-19 11:26:23 +0530

    Time: 20 ms (58.58%), Space: 12.1 MB (11.91%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // 2 pointers lagao bahut famous approach hain woh
+        // other more better is to use maps
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(mp.find(target-nums[i])!=mp.end())
+            {
+                ans.push_back(i);
+                ans.push_back(mp[target-nums[i]]);
+            }
+            else
+            {
+                mp[nums[i]]=i;
+            }
+        }
+        return ans;
+    }
+};
