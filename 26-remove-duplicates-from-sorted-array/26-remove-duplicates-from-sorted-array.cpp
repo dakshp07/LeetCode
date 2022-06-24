3a6a54356151de5c86b1bcf8ecd8734f12b12174
Daksh Paleria <dpaleria@gmail.com>
2022-06-24 12:21:42 +0530

    Time: 29 ms (17.35%), Space: 18.4 MB (74.62%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int curr=0;
-        for(int i=1; i<nums.size(); i++)
+        // one of the ways could be to use some extra space and store stuffs in set
+        // above sol uses extra space, not possible
+        // the other could be to do it in place
+        // optimal is to use 2 pointer:
+        int i=0;
+        for(int j=1; j<nums.size(); j++)
         {
-            if(nums[curr]!=nums[i])
+            if(nums[i]!=nums[j]) // if they are diff
             {
-                nums[++curr]=nums[i];
-                // curr++;
+                // increment i to next
+                i++;
+                nums[i]=nums[j]; // change the value of nums at j to nums at i
             }
         }
-        if(nums.size()==0)
-        {
-            return 0;
-        }
-        return curr+1;
+        return i+1;
     }
 };
