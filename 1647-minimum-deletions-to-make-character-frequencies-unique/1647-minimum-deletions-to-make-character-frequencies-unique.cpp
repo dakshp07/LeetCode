78ddc1f9d04e4851a60806f39b66541f3fd26222
Daksh Paleria <dpaleria@gmail.com>
2022-06-28 09:17:06 +0530

    Time: 68 ms (72.75%), Space: 17.2 MB (95.28%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        // maitain a threshold freq f whch will be the max freq of that string
+        // lets first store the freq of all chars in s
         vector<int> freq(26, 0);
-        for(int i=0; i<s.length(); i++)
+        for(int i=0; i<s.size(); i++)
         {
             freq[s[i]-'a']++;
         }
+        // sort the freq, since we dont see chars we only need freq
         sort(freq.begin(), freq.end(), greater<int>());
-        int f=freq[0];
-        int ans=0;
+        // there will be at max freq of s.length (ie all chars in s are same)
+        int max_freq=s.length();
+        // now for the freq of other chars we need it to be less than max freq
+        int del=0;
         for(int i=0; i<26; i++)
         {
-            if(freq[i]>f)
+            // since we can just delete chars
+            max_freq=min(max_freq, freq[i]);
+            // deletions will be diff b/w frequencies
+            del+=freq[i]-max_freq;
+            // now noother char can have same freq as max_freq
+            if(max_freq>0)
             {
-                if(f>0) ans+=(freq[i]-f);
-                else ans+=freq[i];
+                max_freq--;
             }
-            f=min(f-1, freq[i]-1);
         }
-        return ans;
+        return del;
     }
 };
