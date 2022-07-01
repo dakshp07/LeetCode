7cd9b1e35a6523b058cff36bfed28ed0406f0085
Daksh Paleria <dpaleria@gmail.com>
2022-07-01 11:42:25 +0530

    Time: 410 ms (5.69%), Space: 49.4 MB (14.24%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    // sort karte acc to vec[1]
-    bool static cmpr(vector<int> a, vector<int> b)
+    static bool cmpr(vector<int> a, vector<int> b)
     {
-        return a[1] > b[1];
+        return a[1]>b[1]; // we will keep the box with max no of units in front
     }
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        // sort acc to cmpr
+        // we need to maximize the no of units in truck
+        // lets pick greedily and pick them accordingly
         sort(boxTypes.begin(), boxTypes.end(), cmpr);
-        int ans=0;
+        int res=0;
         for(int i=0; i<boxTypes.size(); i++)
         {
-            // choose min of boxType and truckSize
-            int box=min(boxTypes[i][0], truckSize);
-            ans+=(box*boxTypes[i][1]);
-            truckSize-=box;
-            if(truckSize==0)
-            {
-                break;
-            }
+            // we try to pick minimum boxes that has max units in it so that we can maximize our output
+            int pickBox=min(boxTypes[i][0], truckSize);
+            // we picked up, lets reduce our truck size
+            truckSize-=pickBox;
+            // add our box to ans
+            res+=pickBox*boxTypes[i][1];
+            // if we reach zero in truck size we break
+            if(truckSize==0) break;
         }
-        return ans;
+        return res;
     }
 };
