cac92f392e3f121966899f81e5fd9c7affab9f98
Daksh Paleria <dpaleria@gmail.com>
2022-07-03 13:27:47 +0530

    Time: 3 ms (63.80%), Space: 5.9 MB (31.35%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long res=0;
+        while(x!=0)
+        {
+            res=res*10+x%10;
+            x/=10;
+        }
+        if(res<INT_MIN || res>INT_MAX)
+        {
+            return 0;
+        }
+        return (int)res;
+    }
+};
