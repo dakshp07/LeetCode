9571113e2ee7fa070ff77c080178a44d188e7d6e
Daksh Paleria <dpaleria@gmail.com>
2022-07-04 09:35:55 +0530

    Time: 12 ms (61.09%), Space: 10.4 MB (8.79%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        // sorting karne se O(nlogn) lagega
-        // ek traversal main hoga:
-        // priority queue use karte hain
-        // reason ye ki sab order main rakhegi woh
+        // one of the ways is t sort the array and look for k-1 th ele which is basially kth largest ele
+        // but tc will be O(nlogn) and sc: O(1)
+        // but lets optimise it to O(n) and some sc: O(n)
+        // we can use a heap (ie max heap) to do that and it will arrange ele in increasing order for us
+        // heap insertion is O(1) and access is O(n) which is certainly removing that extra log n in our sorting complexity
         priority_queue<int> pq;
         for(int i=0; i<nums.size(); i++)
         {
+            // insert in queue
             pq.push(nums[i]);
         }
-        k--; // 0 indexing
-        while(k!=0) // pop karo jab taak hum kth tak na aajaye
+        // to make k zero based index we reduce k by one
+        k--;
+        while(k!=0)
         {
+            // we pop until we reach kth ele
             pq.pop();
             k--;
         }
-        return pq.top();
+        return pq.top(); // now top will have the kth largest ele
     }
 };
