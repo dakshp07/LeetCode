cfd00552384a7fba5f12742a610cb2d797a50aab
Daksh Paleria <dpaleria@gmail.com>
2022-07-05 21:46:35 +0530

    Time: 9 ms (59.97%), Space: 9 MB (16.05%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int>stack;
-        map<int,int>map;
-        vector<int>ans;
-        for(int i=0;i<nums2.size();i++){
-            while(!stack.empty()){
-                if(stack.top()<nums2[i]){
-                    map.insert(pair<int,int>(stack.top(),nums2[i]));
-                    stack.pop();
-                }else break;
+        // brute force is to just traverse every ele in nums2 for every ele in nums1
+        // and enter the next greater ele in an array
+        // tc: O(len1*len2) sc: O(1)
+        // the other way is to maintain a stack and keep an increasing sequence in it
+        map<int, int> mp;
+        stack<int> st;
+        vector<int> ans;
+        for(int i=0; i<nums2.size(); i++)
+        {
+            while(!st.empty() && st.top()<nums2[i])
+            {
+                mp[st.top()]=nums2[i];
+                st.pop();
             }
-            stack.push(nums2[i]);
+            st.push(nums2[i]);
         }
-        
-        for(int i=0;i<nums1.size();i++){
-            int val = map[nums1[i]];
-            if(val==0)ans.push_back(-1);
-            else ans.push_back(val);
+        for(int i=0; i<nums1.size(); i++)
+        {
+            if(mp[nums1[i]]==0) ans.push_back(-1);
+            else ans.push_back(mp[nums1[i]]);
         }
         return ans;
     }
