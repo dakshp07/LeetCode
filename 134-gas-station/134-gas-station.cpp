a20faae90931e7c3e8e5a8b1bffac5c113964fd8
Daksh Paleria <dpaleria@gmail.com>
2022-07-10 11:04:14 +0530

    Time: 147 ms (30.61%), Space: 69.4 MB (89.26%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int total=0, curr=0, ans=0;
+        // greedily karenge
+        int pos=0, sum=0, total=0;
         for(int i=0; i<gas.size(); i++)
         {
-            total+=gas[i]-cost[i];
-            curr+=gas[i]-cost[i];
-            if(curr<0)
-            {
-                curr=0;
-                ans=i+1;
-            }
+            // temp variable sum to keep a check if we get <0
+            sum+=(gas[i]-cost[i]);
+            
+            // if our temp gets negative we know our ans is not this index and we add this sum to total to get the point were diff becomes 0 or >0 and then make sum 0 for new traversal
+            if(sum<0) pos=i+1, total+=sum, sum=0;
         }
-        if(total<0)
-        {
-            ans=-1;
-        }
-        return ans;
+        // we add sum to total
+        total+=sum;
+        // if we ever get total as 0 or >0 we know our position is here
+        if(total>=0) return pos;
+        return -1;
     }
 };
