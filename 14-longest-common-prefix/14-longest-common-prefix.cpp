27f1e4e4ae1a2694a7cbc01e9cd55684ac03611b
Daksh Paleria <dpaleria@gmail.com>
2022-07-14 22:28:15 +0530

    Time: 7 ms (54.56%), Space: 9.2 MB (51.61%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans=strs[0];
+        // lets consider the first string in the array as the prefix
+        // and then chop off letters as we move ahead
+        string prefix=strs[0];
         for(int i=1; i<strs.size(); i++)
         {
-            while(strs[i].find(ans)!=0)
+            // lets see if we have common in our string
+            int j=0;
+            while(j<strs[i].length())
             {
-                ans=ans.substr(0, ans.length()-1);
+                // if we have same stuff we continue
+                if(strs[i][j]==prefix[j]) 
+                {
+                    j++;
+                }
+                else break;
             }
+            // we keep that substring
+            prefix=prefix.substr(0, j);
         }
-        return ans;
+        return prefix;
     }
 };
