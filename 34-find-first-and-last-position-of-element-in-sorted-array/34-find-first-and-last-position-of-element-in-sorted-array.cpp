330f35bfbf22f0bb5f1e5f308bf3ccfb90d99f31
Daksh Paleria <dpaleria@gmail.com>
2022-07-25 09:36:34 +0530

    Time: 20 ms (14.10%), Space: 13.5 MB (83.66%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
+    int bs_left(vector<int> &nums, int target)
+    {
+        int n=nums.size();
+        int low=0, high=n-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]<target)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        if(low==nums.size() || nums[low]!=target) return -1;
+        return low;
+    }
+    int bs_right(vector<int> &nums, int target)
+    {
+        int n=nums.size();
+        int low=0, high=n-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        if(high<0 || nums[high]!=target) return -1;
+        return high;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        vector<int> ans(2, -1);
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(nums[i]==target)
-            {
-                ans[0]=i;
-                break;
-            }
-        }
-        for(int i=nums.size()-1 ;i>=0; i--)
-        {
-            if(nums[i]==target)
-            {
-                ans[1]=i;
-                break;
-            }
-        }
+        // ek toh hain ki sort and return the indexes. tc: O(n) and sc: O(1)
+        // binary laga lo, to find the index. tc: O(log n) and sc: O(1)
+        vector<int> ans(2, 0);
+        ans[0]=bs_left(nums, target);
+        ans[1]=bs_right(nums, target);
         return ans;
     }
 };
