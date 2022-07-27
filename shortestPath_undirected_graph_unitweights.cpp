a919cb92f42c81e6c8ac27b888a4db2e95ce5baf
Daksh Paleria <dpaleria@gmail.com>
2022-07-27 11:05:10 +0530

    Update shortestPath_undirected_graph_unitweights.cpp

--- shortestPath_undirected_graph_unitweights.cpp
+++ shortestPath_undirected_graph_unitweights.cpp
@@ -2,6 +2,7 @@ void bfs(vector<int> adj[], int V, int src)
 {
     vector<int> dis(V, INT_MAX); // intialize all the node dis as INT_MAX
     queue<int> q;
+    dis[src]=0; // update the dis of src ie 0
     q.push(src); // push the src into queue
     while(!q.empty())
     {
@@ -9,8 +10,8 @@ void bfs(vector<int> adj[], int V, int src)
         q.pop();
         for(auto it: adj[node])
         {
-            // since the weight is 1 for every node, and we know the dis b/w src and node (ie dis[node]) we update our dis[node] only if its smaller
-            if(dis[node]+1<dis[node])
+            // since the weight is 1 for every node, and we know the dis b/w src and the parent node of this adj node (ie dis[node]) we update our dis[it] only if its smaller
+            if(dis[node]+1<dis[it])
             {
                 dis[it]=dis[node]+1;
                 q.push(it); // we also push only the adj nodes whose dis gets updated
