c09aba58ec0aaa96a7262c40a25b8b59e451d96b
Daksh Paleria <dpaleria@gmail.com>
2022-07-31 19:25:31 +0530

    Time: 4 ms (64.43%), Space: 6.6 MB (75.62%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        stack<int> st;
-        vector<bool> visited(26);
-        vector<int> last_index(26);
+        // we will keep an array to store the frequency
+        // a visited array to keep a check if the the ith char is in resultant string or not
+        vector<int> freq(26, 0);
+        string res="";
+        vector<int> vis(26, 0);
+        // store the freq of all char
         for(int i=0; i<s.length(); i++)
         {
-            last_index[s[i]-'a']=i;
+            freq[s[i]-'a']++;
         }
         for(int i=0; i<s.length(); i++)
         {
-            if(visited[s[i]-'a'])
+            int c=s[i]-'a';
+            // decrease the freq of this char as we pick this
+            freq[c]--;
+            // we check if the char is visited or not
+            if(!vis[c])
             {
-                continue;
+                // then we will see if the last char of res was > then this char
+                // if it was, then we pop back from res until we get a char < then this char
+                // and also mark that char as unvisited
+                while(res.size()>0 && res.back()>s[i] && freq[res.back()-'a']>0)
+                {
+                    vis[res.back()-'a']=0;
+                    res.pop_back();
+                }
+                // if res.back()<s[i], then we add it to res and also mark as visited
+                res+=s[i];
+                vis[c]=1;
             }
-            while(!st.empty() && st.top()>s[i]-'a' && i<last_index[st.top()])
-            {
-                visited[st.top()]=false;
-                st.pop();
-            }
-            st.push(s[i]-'a');
-            visited[s[i]-'a']=true;
         }
-        string res;
-        while(!st.empty())
-        {
-            res.push_back(st.top()+'a');
-            st.pop();
-        }
-        reverse(res.begin(), res.end());
         return res;
     }
 };
