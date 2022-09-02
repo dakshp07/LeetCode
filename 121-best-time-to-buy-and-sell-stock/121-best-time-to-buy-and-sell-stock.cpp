cb64cf7c68ac6e5b064e01c9bf6a5103adefb5ca
Daksh Paleria <dpaleria@gmail.com>
2022-09-02 22:52:29 +0530

    Time: 266 ms (14.28%), Space: 93.4 MB (11.18%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // simple karenge
-        // curr rakho, sum rakho
-        int curr=INT_MAX; // curr ko min karna
-        int sum=INT_MIN; // sum ko maximise karna
+        // maximize karna hain profit
+        // matlab buying price should be as small as possible
+        // but the difference b/w buying and selling should be maximum
+        int buy=INT_MAX;
+        int profit=INT_MIN;
         for(int i=0; i<prices.size(); i++)
         {
-            curr=min(prices[i], curr);
-            sum=max(prices[i]-curr, sum);
+            // minimize buy
+            buy=min(buy, prices[i]);
+            // maximize the profit
+            profit=max(profit, prices[i]-buy);
         }
-        return sum;
+        return profit;
     }
 };
