657717ee63be24d5ef3bd9900950c03dadf81860
Daksh Paleria <dpaleria@gmail.com>
2022-09-14 16:30:08 +0530

    Time: 26 ms (8.30%), Space: 6.9 MB (91.38%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> char_map;
-        unordered_map<char, bool> true_map;
-        for(int i=0; i<s.length(); i++)
+        // we can create a map
+        // and all the character mapping from s to t in it
+        // for every char we check if that specific char has the same mapping
+        unordered_map<char, char> mp;
+        // one set to see the assigned values
+        unordered_set<char> st;
+        for(int i=0; i<s.size(); i++)
         {
-            if(char_map.find(s[i])!=char_map.end())
+            // now we see if s[i] is already in map
+            // if its there we see if its corresponding char ie t[i] is not its mapping
+            // which means s[i] is already mapped to someone which is not s[i]
+            // if thats the case we return false
+            if(mp.find(s[i])!=mp.end() && mp[s[i]]!=t[i])
             {
-                if(char_map[s[i]]!=t[i])
-                {
-                    return false;
-                }
+                return false;
             }
-            else
+            // the other case if s[i] is not in map but its corresponding char t[i] is in set
+            // which means t[i] is already mapped to someone
+            // then too we return false
+            if(mp.find(s[i])==mp.end() && st.find(t[i])!=st.end())
             {
-                if(true_map.find(t[i])!=true_map.end())
-                {
-                    return false;
-                }
-                else
-                {
-                    char_map[s[i]]=t[i];
-                    true_map[t[i]]=true;
-                }
+                return false;
             }
+            // in other cases we map the chars and add t[i] to set
+            mp[s[i]]=t[i];
+            st.insert(t[i]);
         }
-        return true;
+        return true; // if after all iterations we dont violate we return true
     }
 };
