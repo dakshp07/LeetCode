f776af30e4ca9b7a84572c528d433ffe1c893c86
Daksh Paleria <dpaleria@gmail.com>
2022-09-17 13:23:24 +0530

    Time: 11 ms (13.22%), Space: 6.6 MB (81.39%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.cpp
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int res=0;
+        // we need to see the longest palindrome that we can build from the given chars
         vector<int> freq(128, 0);
-        for(int i=0; i<s.length(); i++)
+        for(int i=0; i<s.size(); i++)
         {
             freq[s[i]]++;
         }
-        for(int i=0; i<freq.size(); i++)
+        // we check the chars that have even freq as we can take them
+        // for the non even ones we need to see if our current res is even or not
+        // if its even we take one char from the non even ones
+        int ans=0;
+        for(int i=0; i<128; i++)
         {
-            res+=(freq[i]/2)*2;
-            if(res%2==0 && freq[i]%2==1)
+            // this expression will make sure that we pick even chars from freq
+            ans+=freq[i]/2*2;
+            // if the chars are odd then we pick just one from them
+            if(ans%2==0 && freq[i]%2==1)
             {
-                res+=1;
+                ans++;
             }
         }
-        return res;
+        return ans;
     }
 };
