4d01beb130d3ec3e0767b518733c75473f9b085f
Daksh Paleria <dpaleria@gmail.com>
2022-09-22 12:37:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (39.50%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,19 +1,52 @@
 class Solution {
 public:
     int fib(int n) {
-        // recurive way:
-        // if(n==0) return 0;
-        // if(n==1) return 1;
-        // return fib(n-1)+fib(n-2);
-        
-        // dp way: much efficient since you reduce the tree by saving ans
-        vector<int> dp(31);
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2; i<31; i++)
+        // there are multiple ways to solve this problem
+        // lets start off with recursion and will optmise it
+        /*
+        recursion: tc: O(2^n) and sc: O(n)
+        int func(int n)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+        // base cases
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return func(n-1)+func(n-2);
         }
-        return dp[n];
+        
+        memoization: tc: O(n) and sc: O(n)+O(n)
+        vector<int> dp(31, -1);
+        int func(int n)
+        {
+        if(dp[n]!=-1) return dp[n];
+        // base cases
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return dp[n]=func(n-1)+func(n-2);
+        }
+        */
+        // tabulation: tc: O(n) and sc: O(n) for array
+        // vector<int> dp(31, 0);
+        // base case
+        // dp[0]=0;
+        // dp[1]=1;
+        // for loops
+        // for(int i=2; i<=n; i++)
+        // {
+        //     dp[i]=dp[i-1]+dp[i-2];
+        // }
+        // return dp[n];
+        // space optimized:
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int prev1=0;
+        int prev2=1;
+        int ans=0;
+        for(int i=2; i<=n; i++)
+        {
+            int curr=prev1+prev2;
+            prev1=prev2;
+            prev2=curr;
+        }
+        return prev2;
     }
 };
