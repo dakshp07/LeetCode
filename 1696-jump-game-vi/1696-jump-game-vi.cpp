78eb7f275031e4e556c0fb4a2b1c722f15fa7646
Daksh Paleria <dpaleria@gmail.com>
2022-09-24 18:32:05 +0530

    Time: 535 ms (13.19%), Space: 90.5 MB (42.25%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,27 +1,81 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        // dp se karna hoga
-        // dp+max heap lagao, heap store karega max value of the window i+k
-        // dp[i] will store the max score till ith index
-        priority_queue<pair<int, int>> pq;
-        vector<int> dp(nums.size());
-        // initialize dp
-        dp[nums.size()-1]=nums[nums.size()-1];
-        // initialize heap
-        pq.push(make_pair(dp[nums.size()-1], nums.size()-1));
-        // iterate from nums.size()-2
-        for(int i=nums.size()-2; i>=0; i--)
+        // we will try to explore all paths
+        // lets use recursion
+        /*
+        recursion: tc: exponential and sc: O(path length)
+        // this sum var will be used to calculate the final ans
+        int sum=0;
+        int func(int ind)
         {
-            // now we pop the ele that are exceeding index from our window ie i+k
-            while(pq.size()>0 && pq.top().second>i+k)
-            {
-                pq.pop();
-            }
-            // add the ones that follow our window
-            dp[i]=pq.top().first+nums[i]; // our current index + max of heap ie top of heap
-            pq.push(make_pair(dp[i], i)); // push current score and index in heap
+        // base case
+        if(ind==0) return arr[ind];
+        // explore possibilities
+        for(int next=1; next<=k; next++)
+        {
+        // to make sure we never reach -ve index
+        if(ind-next>=0)
+        {
+        int temp=arr[ind]+func(ind-next);
+        sum=max(sum, temp);
         }
-        return dp[0];
+        }
+        return sum;
+        }
+        
+        memoization: tc: exponential and sc: O(path length) and O(n)
+        vector<int> dp(100001, -1);
+        int sum=0;
+        int func(int ind)
+        {
+        if(dp[ind]!=-1) return dp[ind];
+        // base case
+        if(ind==0) return arr[ind];
+        // explore possibilities
+        for(int next=1; next<=k; next++)
+        {
+        // to make sure we never reach -ve index
+        if(ind-next>=0)
+        {
+        int temp=arr[ind]+func(ind-next);
+        sum=max(sum, temp);
+        }
+        }
+        return dp[ind]=sum;
+        }
+        */
+        // tabulation: O(n*k) and sc: O(n)
+        // int n=nums.size();
+        // vector<int> dp(n, -1e9);
+        // base case
+        // dp[0]=nums[0];
+        // for(int ind=1; ind<n; ind++)
+        // {
+        //     int sum=-1e9;
+        //     for(int next=1; next<=k; next++)
+        //     {
+        //         if(ind-next>=0)
+        //         {
+        //             int temp=nums[ind]+dp[ind-next];
+        //             sum=max(sum, temp);
+        //         }
+        //     }
+        //     dp[ind]=sum;
+        // }
+        // return dp[n-1];
+        
+        // priority queue: tc: O(nlogk) and sc: O(n+n)
+        // so instead of getting max fromm the dp array, we use max heap
+        // and we will pop the heap till we get the next maximum sum
+        priority_queue<pair<int, int>> pq;
+        pq.push({nums[0], 0});
+        
+        for(int i=1;i<nums.size();i++){
+            while(pq.size() && pq.top().second < i-k) pq.pop();
+            pq.push({pq.top().first + nums[i], i});
+        }
+        while(pq.top().second != nums.size()-1) pq.pop();
+        return pq.top().first;
     }
 };
