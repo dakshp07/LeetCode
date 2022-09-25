486c3134222a33db4cdcef7d8d1e7698923e8060
Daksh Paleria <dpaleria@gmail.com>
2022-09-25 10:04:37 +0530

    Time: 13 ms (87.86%), Space: 10.9 MB (32.95%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // 2 pointers lagao bahut famous approach hain woh
-        // other more better is to use maps
+        // brute is to run two loops. tc: O(n^2) and sc: O(1)
+        // one of the standard way is to sort and use a two pointer approach
+        // and see if we get the ans 
+        // tc: O(nlogn) and s : O(1)
+        
+        // but lets see if we can do it off in one traversal
+        // we use a hash map to keep track of ele and their indices
+        // mp<ele,index>
+        // tc: O(n) and sc: O(n)
         unordered_map<int, int> mp;
         vector<int> ans;
         for(int i=0; i<nums.size(); i++)
         {
+            // if nums[i] and target-nums[i] are there in map then we add them to vector and break
             if(mp.find(target-nums[i])!=mp.end())
             {
-                ans.push_back(i);
                 ans.push_back(mp[target-nums[i]]);
+                ans.push_back(i);
+                return ans;
             }
-            else
-            {
-                mp[nums[i]]=i;
-            }
+            // or else we add the nums[i] to our map
+            mp[nums[i]]=i;
         }
         return ans;
     }
