a2d81f5077687758d0cbd63e8b026f581253f072
Daksh Paleria <dpaleria@gmail.com>
2022-09-25 10:16:39 +0530

    Time: 6 ms (64.42%), Space: 6.5 MB (74.47%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        // map main nunber, freq daal dunga
-        // maan lo if at i pos we dont have a specific dig then we see in map
-        // agar same pos pe aagaya toh thik hi hain
+        // we will store the secret in our map
+        // map<char,freq> and then iterate over both the strings
+        // if at a position the char matches we put them as bulls and reduce fre by 1
+        // and similarly we do for cows
         unordered_map<char, int> mp;
+        // store secret freq
         for(int i=0; i<secret.size(); i++)
         {
             mp[secret[i]]++;
         }
-        int i=0, j=0;
-        int bulls=0, cows=0;
+        // now iterate to find bulls
+        int bulls=0;
         for(int i=0; i<secret.size(); i++)
         {
+            // if both char match we increment bulls counter and decrease its freq by 1
             if(secret[i]==guess[i])
             {
                 bulls++;
-                mp[guess[i]]--;
+                mp[secret[i]]--;
             }
         }
+        // now iterate to find cows
+        int cows=0;
         for(int i=0; i<secret.size(); i++)
         {
+            // if both char dont match
+            // but we have the guess char in our map with freq >0
+            // we increment our cows count
             if(secret[i]!=guess[i] && mp[guess[i]]>0)
             {
                 cows++;
                 mp[guess[i]]--;
             }
         }
-        string ans="";
-        ans+=to_string(bulls);
-        ans+='A';
-        ans+=to_string(cows);
-        ans+='B';
+        // frame ans
+        string ans=to_string(bulls)+'A'+to_string(cows)+'B';
         return ans;
     }
 };
