27b35e9e02faf510435e8dc3f4c60bf116c68844
Daksh Paleria <dpaleria@gmail.com>
2022-09-27 09:48:14 +0530

    Time: 30 ms (24.00%), Space: 12.7 MB (54.71%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    static bool compare(pair<int,string> a,pair<int,string> b){
-        if(a.first==b.first){
-            return a.second>b.second;
-        }
-        return a.first<b.first;
-    }
+    struct cmpr{
+		bool operator()(pair<int,string> a, pair<int,string> b) const{
+			if(a.first==b.first) return a.second<b.second;
+			return a.first>b.first;
+		}
+	};
     vector<string> topKFrequent(vector<string>& words, int k) {
-        map<string,int> mp;
-        for(auto word:words){
-            mp[word]++;
+        // there are two ways to solve problem:
+        // 1. use map and then use a cmpr on vec. tc: O(logn)+O(nlogn)+O(logn)
+        // 2. use map and then use a cmpr on heap. tc: O(logn)+O(logn)+O(1) 
+        // so we use a map <string,freq> and a heap of pairs <freq,words>
+        // heap will help in keeping track of most freq ele
+        unordered_map<string,int> mp;
+        for(int i=0; i<words.size(); i++)
+        {
+            mp[words[i]]++;
         }
-        map<string,int>::iterator it;
-        vector<pair<int,string>>v1;
-        for(it=mp.begin();it!=mp.end();it++){
-            
-            v1.push_back(make_pair(it->second,it->first));
+        // we use comp on the heap to sort them acc to lexi if freq is same
+        priority_queue<pair<int,string>, vector<pair<int, string>>, cmpr> pq;
+        for(auto it: mp)
+        {
+            pq.push({it.second, it.first});
+            if(pq.size()>k) pq.pop();
         }
-        sort(v1.begin(),v1.end(),compare);
         vector<string> ans;
-        int cnt=0;
-        for(int i=v1.size()-1;i>=0;i--){
-            if(cnt==k){
-                break;
-            }cnt++;
-            
-            ans.push_back(v1[i].second);
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
+        // reverse the vec
+        reverse(ans.begin(), ans.end());
         return ans;
-        
     }
 };
