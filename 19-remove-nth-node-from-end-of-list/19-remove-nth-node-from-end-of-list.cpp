ec57dddf1088053a5f135d9efefb528db3628c3e
Daksh Paleria <dpaleria@gmail.com>
2022-09-29 11:12:03 +0530

    Time: 13 ms (14.59%), Space: 10.8 MB (32.19%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,22 +11,34 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // slow or fast rakho
-        // and delete
-        ListNode *start=new ListNode;
-        start->next=head;
-        ListNode *fast=start;
-        ListNode *slow=start;
+        // one of the ways could be to find the length of list
+        // and then trace back to the nth node
+        // and change their connections
+        // tc: O(len of list)+O(n)
+        // lets reduce the time complexity and do it off in one traversal
+        
+        // we use slow, fast pointer
+        ListNode *slow, *fast, *temp;
+        temp=new ListNode();
+        temp->next=head; // temp starts from head
+        // both slow and fast are at temp position
+        fast=temp;
+        slow=temp;
+        // fast will start from nth pos
         for(int i=0; i<n; i++)
         {
-            fast=fast->next; // fast starts at same step as n
+            fast=fast->next;
         }
+        // now we run slow, fast till fast->next is null
         while(fast->next!=NULL)
         {
-            fast=fast->next;
             slow=slow->next;
+            fast=fast->next;
         }
+        // now slow is at the position where the nth node is
+        // so we remove connection
         slow->next=slow->next->next;
-        return start->next;
+        // return temp->next node back as thats our head
+        return temp->next;
     }
 };
