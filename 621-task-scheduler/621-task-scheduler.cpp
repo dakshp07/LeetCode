54096a41dda5178732492f7b64de9d42f27c89a0
Daksh Paleria <dpaleria@gmail.com>
2022-10-01 13:34:21 +0530

    Time: 177 ms (34.81%), Space: 34.3 MB (91.57%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int> freq_alpha(26);
+        // lets store all tasks in an fre arr
+        vector<int> freq(26, 0);
         for(int i=0; i<tasks.size(); i++)
         {
-            freq_alpha[tasks[i]-'A']++;
+            freq[tasks[i]-'A']++;
         }
-        sort(freq_alpha.begin(), freq_alpha.end());
-        int max_val=freq_alpha[25]-1;
-        int idle=max_val*n;
+        // now we sort the array to get the max value at end
+        sort(freq.begin(), freq.end());
+        // now the max value will be at an index 25
+        int max_val=freq[25]-1; // we subtract one because once we reach to the last task in tasks array we dont have wait
+        // so for every task the no of slots we need to fill is at max max_val*n
+        int idle_slots=max_val*n;
+        // now we assume that all these slots are idle
+        // now we fill these slots with tasks
+        // loop for behind of freq array
         for(int i=24; i>=0; i--)
         {
-            idle-=min(freq_alpha[i], max_val);
+            idle_slots-=min(freq[i], max_val);
         }
-        return idle>0?idle+tasks.size():tasks.size();
+        // so now ans will be
+        // if there are any idle slots then ans is the length of task array
+        // or else the ans will be idle_slot+task arr length
+        return idle_slots>0 ? idle_slots+tasks.size() : tasks.size();
     }
 };
