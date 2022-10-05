52de1e8732be42396ddd29a407e4e4d579cfdf5b
Daksh Paleria <dpaleria@gmail.com>
2022-10-05 11:54:35 +0530

    Time: 30 ms (58.47%), Space: 24.1 MB (60.03%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -13,24 +13,23 @@ class Solution {
 public:
     int ans=0;
     int cnt=0;
-    void inorder(TreeNode *root, int k)
+    void inorder(TreeNode* root, int k)
     {
         // LrootR
         if(root==NULL) return;
-        // int cnt=0; // we check through this
         inorder(root->left, k);
+        // we do cnt++ everytime
         cnt++;
-        if(cnt==k) ans=root->val; // since the inorder is sorted order, we return when cnt == k
+        // we store answer when cnt==k
+        if(cnt==k)
+        {
+            ans=root->val;
+        }
         inorder(root->right, k);
     }
     int kthSmallest(TreeNode* root, int k) {
-        // one way is to traverse and store the values heap
-        // and return k-1 ele from heap
-        // tc: O(n) for tree traversal and O(1) for insert and O(n) for accessing k-1 ele from heap
-        // tc: O(n)
-        // sc:O(n)
-        // we can reduce the sc by keepina a count and doing inorder traversal
-        // inorder is always sorted for bst and when count == k we return
+        // we know that inorder of bst is always sorted
+        // so we do an recursive inorder and at the same time keep track of cnt==k
         inorder(root, k);
         return ans;
     }
