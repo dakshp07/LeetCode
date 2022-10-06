cc5b11e57219f9d26d6a3a0e473fc5ed7048f1cf
Daksh Paleria <dpaleria@gmail.com>
2022-10-06 13:49:57 +0530

    Time: 11 ms (66.90%), Space: 13.1 MB (80.67%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,59 +1,73 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        // we will use BFS here beacuse in BFS we first visit all adj nodes
-        // whereas in DFS we call adj node, they call their adj nodes and so on
-        // here the rooten ornage destroys the adj ones first, then destroy next one
-        queue<pair<pair<int, int>, int>> q; // stores i, j and current time
-        int fresh=0; // we store fresh count
-        int time=0;
+        // the main catch here is to use bfs rather than dfs
+        // the reason to use bfs is we need to change the adjacent node at the same time
+        // whereas in dfs due to recursion they get change one by one after each recursion call backtracks
+        // apart from the grid val we will also keep a track of minutes in our queue
+        // queue: {{i,j}, minutes}
+        queue<pair<pair<int, int>, int>> q;
+        // now we need to see the cases of fresh oranges still there, as we need -1 to be returned in that case
+        // so we keep track of fresh ornages in grid
+        int fresh=0;
+        int tt=0;
         for(int i=0; i<grid.size(); i++)
         {
             for(int j=0; j<grid[0].size(); j++)
             {
-                if(grid[i][j]==1) fresh++; // keep fresh orange count
-                if(grid[i][j]==2) q.push({{i, j}, 0}); // as we are yet to visit adj node so time=0
+                if(grid[i][j]==1) fresh++;
+                // now we push all rotten oranges in our grid to queue
+                // as we check their adj nodes
+                // time will be 0 as we are yet to start
+                if(grid[i][j]==2) q.push({{i,j}, 0});
             }
         }
-        // BFS
+        // now we start bfs by peforming it till queue is empty
         while(!q.empty())
         {
-            // get all the values from the front of queue
+            // now we pop and store the values
             auto it=q.front();
-            int row=it.first.first;
-            int col=it.first.second;
-            time=it.second;
             q.pop();
-            // we go up, down, left, right to check the fresh ones and make them rotten
-            // at the same time we decrease the value of fresh orange
+            int x=it.first.first; // x coordinate
+            int y=it.first.second; // y coordinate
+            tt=it.second; // we store time as global var as we need to return it
+            // now we go up,down,left,right and mark the fresh oranges as rotten
             
-            if(row!=grid.size()-1 && grid[row+1][col]==1) // down
+            // up
+            if(x-1>=0 && grid[x-1][y]==1) // to check if are not out of bound and grid has fresh orange at this pos
             {
-                fresh--; // decrement fresh
-                grid[row+1][col]=2; // mark grid as visited
-                q.push({{row+1, col}, time+1}); // push to queue as we do in BFS and also add time+1
+                // we push this ele on queue and also increment time by 1 as given in question
+                q.push({{x-1, y}, tt+1});
+                fresh--; // as now fresh count has decreased by one
+                grid[x-1][y]=2; // we mark the grid as visited by making it 2 (rotten)
             }
-            if(row!=0 && grid[row-1][col]==1) // up
+            // down
+            if(x+1<=grid.size()-1 && grid[x+1][y]==1) // to check if are not out of bound and grid has fresh orange at this pos
             {
-                fresh--; // decrement fresh
-                grid[row-1][col]=2; // mark grid as visited
-                q.push({{row-1, col}, time+1}); // push to queue as we do in BFS and also add time+1
+                // we push this ele on queue and also increment time by 1 as given in question
+                q.push({{x+1, y}, tt+1});
+                fresh--; // as now fresh count has decreased by one
+                grid[x+1][y]=2; // we mark the grid as visited by making it 2 (rotten)
             }
-            if(col!=grid[0].size()-1 && grid[row][col+1]==1) // right
+            // left
+            if(y-1>=0 && grid[x][y-1]==1) // to check if are not out of bound and grid has fresh orange at this pos
             {
-                fresh--; // decrement fresh
-                grid[row][col+1]=2; // mark grid as visited
-                q.push({{row, col+1}, time+1}); // push to queue as we do in BFS and also add time+1
+                // we push this ele on queue and also increment time by 1 as given in question
+                q.push({{x, y-1}, tt+1});
+                fresh--; // as now fresh count has decreased by one
+                grid[x][y-1]=2; // we mark the grid as visited by making it 2 (rotten)
             }
-            if(col!=0 && grid[row][col-1]==1) // left
+            // right
+            if(y+1<=grid[0].size()-1 && grid[x][y+1]==1) // to check if are not out of bound and grid has fresh orange at this pos
             {
-                fresh--; // decrement fresh
-                grid[row][col-1]=2; // mark grid as visited
-                q.push({{row, col-1}, time+1}); // push to queue as we do in BFS and also add time+1
+                // we push this ele on queue and also increment time by 1 as given in question
+                q.push({{x, y+1}, tt+1});
+                fresh--; // as now fresh count has decreased by one
+                grid[x][y+1]=2; // we mark the grid as visited by making it 2 (rotten)
             }
         }
-        // if there are still some fresh oranges then we say -1
-        if(fresh!=0) return -1;
-        return time; // if no fresh oranges then we return our total time
+        // now we return our tt if fresh==0
+        // else we return -1
+        return fresh==0 ? tt:-1; 
     }
 };
