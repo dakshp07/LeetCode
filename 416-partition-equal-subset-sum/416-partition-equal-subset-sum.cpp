b8931a60e1e5ffc14ca8b14a59f137f285c8045d
Daksh Paleria <dpaleria@gmail.com>
2022-10-09 12:40:24 +0530

    Time: 809 ms (14.12%), Space: 85 MB (26.59%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,73 +1,73 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        // so the sum of 1 subset should be total_sum/2 then only 2 subsets will add to become sum
-        // so we just need to see if there's a subset which has a sum of total_sum/2
-        // now this question becomes equal to the last question
+        // now we need to partition the array in two equal subsets which have same sum
+        // so if the sum is not divisble by 2 ie sum%2==0 then we return false
+        // else we need to identify one subset that can have a sum of sum/2
+        // beacuse if there's one partition with a sum of sum/2 then the other remaining ele can form a partition of sum=sum/2
+        // so in order to pick the partition we need to explore all options ie either pick a specific ele or not pick it
+        // we use recursion and further optimise it
         /*
-        // recursion: tc: O(2^n) and sc: O(n)
+        recursion: tc: O(2^n) and sc: O(target)
+        here target=sum/2
         bool func(int ind, int target)
         {
-        // base case if the target becomes 0 then we return the true as we reached our goal
-        if(target==0) return true;
-        // else if index is 0 then we check if arr[ind] is equal to target or not
-        if(ind==0) return (arr[ind]==targt);
-        bool not_take=func(i-1, target);
-        bool take=false;
-        // now we check if the arr[ind]<=target or not then we pick it
-        // and call func for other ele
-        if(arr[ind]<=target) take=func(ind-1, target-arr[ind]);
-        return (take || not_take);
+        // base case
+        if(ind==0)
+        {
+        if(arr[ind]!=target) return false;
+        else return true;
         }
-        
-        // memoization: O(n*target) and sc: O(n*target) and O(n) for recursion
-        vector<vector<int>>dp(n, vector<int>(target, -1));
+        // explore all possibilities
+        if(arr[ind]<=target) bool pick=func(ind-1, target-arr[ind]);
+        bool not_pick=func(ind-1, target);
+        return pick || not_pick;
+        }
+
+        memoization: tc: O(n) and sc: O(target)+O(n*target) for dp array
+        vector<vector<int>> dp(n, vector<int>(target+1, -1));
         bool func(int ind, int target)
         {
         if(dp[ind][target]!=-1) return dp[ind][target];
-        // base case if the target becomes 0 then we return the true as we reached our goal
-        if(target==0) return true;
-        // else if index is 0 then we check if arr[ind] is equal to target or not
-        if(ind==0) return (arr[ind]==targt);
-        bool not_take=func(i-1, target);
-        bool take=false;
-        // now we check if the arr[ind]<=target or not then we pick it
-        // and call func for other ele
-        if(arr[ind]<=target) take=func(ind-1, target-arr[ind]);
-        return dp[ind][target]=(take || not_take);
+        // base case
+        if(ind==0)
+        {
+        if(arr[ind]!=target) return false;
+        else return true;
+        }
+        // explore all possibilities
+        if(arr[ind]<=target) bool pick=func(ind-1, target-arr[ind]);
+        bool not_pick=func(ind-1, target);
+        return dp[ind][target]=pick || not_pick;
         }
         */
-        // tabulation: O(n*target) + O(n) for sum calculation and sc: O(n*target)
+        // tabulation: tc: O(n*target) and sc: O(n*target);
         int n=nums.size();
-        // lets calculate sum first
         int sum=0;
         for(int i=0; i<n; i++) sum+=nums[i];
-        // if sum is odd then we cant do it /2 so we return false
-        if(sum%2) return false;
-        int target=sum/2;
-        vector<vector<bool>> dp(n, vector<bool>(target+1, 0));
+        if(sum%2!=0) return false;
+        int result=sum/2;
+        vector<vector<int>> dp(n, vector<int>(result+1, 0));
         // base case
-        // we can have target 0 at all indexes, so we make all index with target 0 as true
-        for(int i=0; i<n; i++)
+        // when ind==0, target can be anything from 0 to target
+        for(int target=0; target<=result; target++)
         {
-            dp[i][0]=true;
+            if(nums[0]!=target) dp[0][target]=false;
+            else dp[0][target]=true;
         }
-        // now about index being 0 we see the target value and nums[0]
-        if(nums[0]<=target) dp[0][nums[0]]=true;
-        // loops for our states, ie 2 loops
-        for(int i=1; i<n; i++)
+        // for loops
+        for(int ind=1; ind<n; ind++)
         {
-            for(int curr_target=1; curr_target<=target; curr_target++)
+            for(int target=0; target<=result; target++)
             {
-                // now we pick or not pick
-                bool not_take=dp[i-1][curr_target];
-                bool take=false;
-                if(curr_target>=nums[i]) take=dp[i-1][curr_target-nums[i]];
-                dp[i][curr_target]=(take or not_take);
+                // explore all possibilities
+                bool pick=false;
+                bool not_pick=false;
+                if(nums[ind]<=target) pick=dp[ind-1][target-nums[ind]];
+                not_pick=dp[ind-1][target];
+                dp[ind][target]=pick || not_pick;
             }
         }
-        // if we were able to find a subset with sum as target we return true
-        if(dp[n-1][target]) return true;
-        return false;
+        return dp[n-1][result];
     }
 };
