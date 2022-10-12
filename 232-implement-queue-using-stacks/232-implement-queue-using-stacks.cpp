1512ba473fd3915d261139755b6c414bb70a789b
Daksh Paleria <dpaleria@gmail.com>
2022-10-12 11:57:58 +0530

    Time: 0 ms (100.00%), Space: 7 MB (89.50%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -1,44 +1,47 @@
 class MyQueue {
 public:
-    // we can use 2 stacks, s1 and s2
-    // when we push:
-    // 1. transfer stuff from s1 to s2
-    // 2. push x to s1
-    // 3. transfer stuff from s2 to s1
-    stack<int> s1, s2;
+    // we can use two stacks
+    // and as soon as we go to pop operation we push all the content from stack1 to stack2
+    // and then pop from stack2
+    stack<int> st1, st2;
     MyQueue() {
-        
     }
     
     void push(int x) {
-        // step 1
-        while(!s1.empty())
-        {
-            s2.push(s1.top());
-            s1.pop();
-        }
-        // step 2
-        s1.push(x);
-        // step 3
-        while(!s2.empty())
-        {
-            s1.push(s2.top());
-            s2.pop();
-        }
+        st1.push(x);
     }
     
     int pop() {
-        int n=s1.top();
-        s1.pop();
-        return n;
+        // push all content from st1 to st2
+        if(st2.empty())
+        {
+            while(!st1.empty())
+            {
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        int tp=st2.top();
+        st2.pop();
+        return tp;
     }
     
     int peek() {
-        return s1.top();
+        // push all content from st1 to st2
+        if(st2.empty())
+        {
+            while(!st1.empty())
+            {
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        return st2.top();
     }
     
     bool empty() {
-        return s1.empty();
+        // we return true if both are empty
+        return st1.empty() && st2.empty();
     }
 };
 
