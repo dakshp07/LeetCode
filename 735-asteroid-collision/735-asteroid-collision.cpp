4b5eaa7be2046b030933bdab14078be94e351f82
Daksh Paleria <dpaleria@gmail.com>
2022-10-14 12:38:39 +0530

    Time: 35 ms (26.18%), Space: 17.9 MB (39.47%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -1,50 +1,36 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& nums) {
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        // so we can use a stack
+        // and push the ele into the stack
+        // and just implement the rules, and pop accordingly
+        // end answer will be present in our stack
         stack<int> st;
-        vector<int> res(st.size());
-        for(int i=0; i<nums.size(); i++)
+        for(int i=0; i<asteroids.size(); i++)
         {
-            if(st.empty() || nums[i]>0)
-            {
-                st.push(nums[i]);
-            }
+            // we push all +ve in stack
+            if(st.empty() || asteroids[i]>0) st.push(asteroids[i]);
+            // now when we see a -ve ele we peform the collision acc to rules
             else
             {
-                while(true)
+                // case 1: when the current asteroid is bigger then previous ones
+                while(!st.empty() && st.top()>0 && st.top()<abs(asteroids[i]))
                 {
-                    if(st.top()<0)
-                    {
-                        st.push(nums[i]);
-                        break;
-                    }
-                    else if(st.top()==-nums[i])
-                    {
-                        st.pop();
-                        break;
-                    }
-                    else if(st.top()>-nums[i])
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        st.pop();
-                        if(st.empty())
-                        {
-                            st.push(nums[i]);
-                            break;
-                        }
-                    }
+                    st.pop();
                 }
+                // case 2: when the current asteroid is same size as previous ones
+                if(!st.empty() && st.top()==abs(asteroids[i])) st.pop();
+                // case 3: when the top of stack is also negative or st is empty
+                else if(st.empty() || st.top()<0) st.push(asteroids[i]);
             }
         }
+        vector<int> ans;
         while(!st.empty())
         {
-            res.push_back(st.top());
+            ans.push_back(st.top());
             st.pop();
         }
-        reverse(res.begin(), res.end());
-        return res;
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
