e520507ce16fcabee13872b87820f59d42023e24
Daksh Paleria <dpaleria@gmail.com>
2022-10-20 13:17:25 +0530

    Time: 6 ms (11.40%), Space: 6.2 MB (99.32%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        // formula for calculating pascal triangle value for a given row r ad col c is:
+        // normal gyaan:
+        // formula for calculating pascal triangle value for a given row r and col c is:
         // r-1 C c-1
         // where, nCr=n!/(n-r)!*r!
-        vector<vector<int>> res(numRows);
-        for(int i=0; i<numRows; i++)
+        
+        // solution:
+        // we just add the top two ele everytime
+        // ie mat[r-1][c]+mat[r-1][c-1]
+        vector<vector<int>> ans;
+        for(int i=0; i<numRows; i++) // at max we will have numRows rows
         {
-            res[i].resize(i+1);
-            res[i][0]=res[i][i]=1;
+            // every row has i+1 ele, we intialize the array with 1s
+            vector<int> row(i+1, 1);
+            // now we traverse from 1st index till ith index as 0th index will always be 1 
             for(int j=1; j<i; j++)
             {
-                res[i][j]=res[i-1][j-1]+res[i-1][j];
+                // for any jth ele the value will be mat[r-1][c]+mat[r-1][c-1]
+                row[j]=ans[i-1][j]+ans[i-1][j-1];
             }
+            // at the end we push every row to ans matrix
+            ans.push_back(row);
         }
-        return res;
+        return ans;
     }
 };
