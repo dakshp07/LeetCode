222e9041a5eb6f9b3ecbc50d32ff318f53bf6c46
Daksh Paleria <dpaleria@gmail.com>
2022-10-22 13:06:38 +0530

    Time: 28 ms (91.20%), Space: 10.7 MB (73.08%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         vector<int> freq(26, 0);
-        // freq store karlo
         for(int i=0; i<s.length(); i++)
         {
             freq[s[i]-'a']++;
         }
-        // iterate on string chars and see if we have any char whose freq is 1
         for(int i=0; i<s.length(); i++)
         {
             if(freq[s[i]-'a']==1)
