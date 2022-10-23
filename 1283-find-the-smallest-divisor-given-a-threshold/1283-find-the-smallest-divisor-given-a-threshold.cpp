d5b15b1db7e792930cbda3e65c3ee36262fe7ecc
Daksh Paleria <dpaleria@gmail.com>
2022-10-23 20:18:23 +0530

    Time: 62 ms (82.10%), Space: 22.4 MB (23.32%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
+    bool possibleAns(int mid, vector<int>& nums, int threshold)
+    {
+        int sums=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            // since the example 1 says we take > or =. eg: 7/3 = 3. so we take ceil
+            sums+=ceil((double)nums[i]/mid);
+        }
+        // if sums<=threshold (given in question) we retun true
+        if(sums<=threshold) return true;
+        // else return false
+        return false;
+    }
     int smallestDivisor(vector<int>& nums, int threshold) {
-        // so the max divisior that we can have is the largest ele in the array
-        // beacause if we divide all ele by max ele in the array then the ans will be 1*size of arr
-        // so now our search range is [1,max_ele_in_array]
-        // if we increase the divisor value our sum decreases and vice versa
-        // we can use binary search
+        // so here we can have many possible answers
+        // which means we can peform a bs on ans space
+        // our range of ss will go from 1 to max(nums)
+        // because the best case would be when threshold is sum(nums) then i divide all by 1
+        // the next case would be when my threshold is 1 so i divide all by max(nums)
+        // hence our ans lies b/w [1,max(nums)]
+        // a checker function will make sure that we pick minimum possible ans
         int left=1;
         int right=*max_element(nums.begin(), nums.end());
+        int ans=right;
         while(left<=right)
         {
             int mid=(left+right)/2;
-            int sum=0;
-            for(int i=0; i<nums.size(); i++)
+            if(possibleAns(mid, nums, threshold))
             {
-                sum+=ceil((double)nums[i]/mid);
-            }
-            if(sum>threshold)
-            {
-                left=mid+1;
+                ans=mid;
+                // we keep on looking on left for more small ans
+                right=mid-1;
             }
             else
             {
-                right=mid-1;
+                left=mid+1;
             }
         }
-        return left;
+        return ans;
     }
 };
