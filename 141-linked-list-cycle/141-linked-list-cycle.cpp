28d7172ddc1ac051ceb8aefee0828e6d5af86c6f
Daksh Paleria <dpaleria@gmail.com>
2022-10-23 11:16:03 +0530

    Time: 11 ms (90.81%), Space: 7.8 MB (95.73%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,19 +9,35 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // extra set ka use karenge
-        // set main saare nodes daalte jaayenge agar kahi same node aaya joh set main hain toh true
-        unordered_set<ListNode*> st; // set store karega
-        ListNode *temp=head; // start traverse
-        while(temp!=NULL) // traverse till list ends
+        // one of the ways is to use a set and put all nodes in it
+        // if we come across any node which is already present in our set we return true
+        // tc: O(len of list) and sc: O(n)
+        
+        // unordered_set<ListNode*> st;
+        // ListNode *temp=head;
+        // while(temp->next!=NULL)
+        // {
+        //     if(st.find(temp)!=st.end())
+        //     {
+        //         return true;
+        //     }
+        //     st.insert(temp);
+        //     temp=temp->next;
+        // }
+        // return false;
+        
+        
+        // but we can use the tortoise hore algo
+        // by keeping a fast, slow ptr. if they meet we return true
+        if(head==NULL) return false;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast!=NULL && fast->next!=NULL)
         {
-            if(st.find(temp)!=st.end()) // agar set main tha
-            {
-                return true; // return true
-            }
-            st.insert(temp); // insert the node in set
-            temp=temp->next; // update temp to next
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast) return true;
         }
-        return false; // no cycle toh false
+        return false;
     }
 };
