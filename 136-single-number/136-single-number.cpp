a6a0b06f93f930b63d8e01b93100d56772c60088
Daksh Paleria <dpaleria@gmail.com>
2022-10-30 21:42:07 +0530

    Time: 51 ms (38.57%), Space: 16.9 MB (52.76%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if(nums.size()==1)
+        // we can use map to put num,freq in it
+        // but that violates const space
+        // we can also sort but that violates linear time complexity
+        // so we use XOR
+        // all the same numbers XOR is 0
+        // at the end we will only have the number which comes once
+        for(int i=1; i<nums.size(); i++)
         {
-            return nums[0];
+            nums[i]=nums[i]^nums[i-1];
         }
-        sort(nums.begin(), nums.end());
-        int i=0;
-        while(i<nums.size())
-        {
-            if(nums[i]==nums[i+1])
-            {
-                i+=2;
-            }
-            else
-            {
-                return nums[i];
-            }
-        }
-        return 0;
+        return nums[nums.size()-1]; // since rest all places are filled with other numbers
     }
 };
