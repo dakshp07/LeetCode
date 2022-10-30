26e88e178c7c97d4910a94c9ebaed23586b15c9a
Daksh Paleria <dpaleria@gmail.com>
2022-10-30 13:27:03 +0530

    Time: 82 ms (56.47%), Space: 36.6 MB (89.37%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,59 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class BSTIterator{
-    stack<TreeNode *> st;
-    // reverse=true -> next()
-    // reverse=fale -> before()
-    bool reverse=true;
-public:
-    BSTIterator(TreeNode *root, bool isReverse)
-    {
-        reverse=isReverse;
-        pushAll(root);
-    }
-    bool hasNext(){
-        return st.size();
-    }
-    int next(){
-        TreeNode *tp=st.top();
-        st.pop();
-        if(!reverse) pushAll(tp->right);
-        else pushAll(tp->left);
-        return tp->val;
-    }
-    void pushAll(TreeNode *root)
-    {
-        while(root!=NULL)
-        {
-            st.push(root);
-            if(reverse==true)
-            {
-                root=root->right;
-            }
-            else
-            {
-                root=root->left;
-            }
-        }
-    }
-};
 class Solution {
 public:
+    void inorder(TreeNode* root, vector<int> &ans)
+    {
+        // inorder: LrootR
+        if(root==NULL) return;
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
+    }
     bool findTarget(TreeNode* root, int k) {
-        // we use the concept of BST Iterator
-        // we will use the next() from it and also create somthing called before()
-        // if sum>k we call next() or else we call before()
-        if(root==NULL) return false;
-        BSTIterator l(root, false);
-        BSTIterator r(root, true);
-        int i=l.next();
-        int j=r.next();
-        while(i<j)
+        // an inorder of bst is always sorted
+        // so we can do an inorder store it in vector and peform a bs on it
+        // the other way could be to use bst iterator (done in previous submission)
+        // we try inorder one
+        vector<int> arr;
+        inorder(root, arr);
+        int left=0, right=arr.size()-1;
+        while(left<right)
         {
-            if(i+j==k) return true;
-            else if(i+j<k) i=l.next();
-            else j=r.next();
+            if(arr[left]+arr[right]==k) return true;
+            if(arr[left]+arr[right]<k) left++;
+            else right--;
         }
         return false;
     }
