af6a8f564280f9419021199dd95f49e594abfe7c
Daksh Paleria <dpaleria@gmail.com>
2022-11-01 13:24:13 +0530

    Time: 10 ms (9.63%), Space: 7 MB (74.05%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        // transpose karo
-        // phir every row ko individually reverse maardo
+        // basicallu we do a transpose
+        // and then reverse every row
+        // ex: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+        // transpose: [[1,4,7], [2,5,8], [3,6,9]]
+        // reverse every row: [[7,4,1], [8,5,2], [9,6,3]]
+        // which is our desired result
+        
+        // transpose
         for(int i=0; i<matrix.size(); i++)
         {
             for(int j=0; j<i; j++)
             {
-                swap(matrix[i][j], matrix[j][i]); // making rows to go to column
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        // now reverse every row
+        // reverse every row
         for(int i=0; i<matrix.size(); i++)
         {
             reverse(matrix[i].begin(), matrix[i].end());
