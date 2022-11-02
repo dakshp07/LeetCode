0ee477ff3960d4b18c1cef7c4cda63e6af9ea99f
Daksh Paleria <dpaleria@gmail.com>
2022-11-02 12:50:51 +0530

    Time: 1002 ms (32.95%), Space: 89.8 MB (81.84%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
+    static bool cmp(vector<int> &a, vector<int> &b)
+    {
+        return a[1]<b[1]; // sort on end time basis
+        // so we sort on ascending basis of end
+        // [1,2],[2,3],.....
+    }
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        int count=0;
-        int prev=intervals[0][1];
+        // sort the intervals on basis of end time and we start traversing
+        // and cnt will intially be equal to 0
+        // as we find overlapping intervals we increment the cnt
+        sort(intervals.begin(), intervals.end(), cmp);
+        int cnt=0; 
+        vector<int> prev=intervals[0];
         for(int i=1; i<intervals.size(); i++)
         {
-            if(intervals[i][0]<prev)
+            // now we dont need to check end time
+            // as we sorted on that basis
+            // we check start time, if any interval start time is <prev end time
+            // means they overlap
+            if(prev[1]>intervals[i][0])
             {
-                count++;
-                prev=min(prev, intervals[i][1]);
+                cnt++;
             }
+            // if they dont overlap then we update our prev
             else
             {
-                prev=intervals[i][1];
+                prev=intervals[i];
             }
         }
-        return count;
+        return cnt;
     }
 };
