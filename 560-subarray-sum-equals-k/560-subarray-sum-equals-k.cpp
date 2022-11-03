4639d75021175080356bee261e965f85a373efbd
Daksh Paleria <dpaleria@gmail.com>
2022-11-03 13:16:02 +0530

    Time: 236 ms (15.95%), Space: 43.8 MB (35.18%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
+        // brute force would be to find all subarrays using recursion
+        // and then iterate over them to see how many have sum==k
+        
+        // we can use a map to optimise it
+        // in map we store the number of times we saw a sum
+        // so lets say if we find a subarray with sum x
+        // and another subarray of sum x+k
+        // then the subarray lying in b/w the these two will have sum of k
+        // so we store how many times we saw a sum and look for how many times we say sum-k
         map<int, int> mp;
-        int count=0, sum=0;
-        mp[0]=1; // storing the count of times when i saw a sum
-        for(int i=0; i<n; i++)
+        mp[0]=1; // we saw sum of 0 just once
+        int cnt=0, sum=0;
+        for(int i=0; i<nums.size(); i++)
         {
             sum+=nums[i];
+            // since we already have a subarray with sum as sum
+            // we look for sum-k in our map
+            // if it exists we add the corresponding map value in cnt
             if(mp.find(sum-k)!=mp.end())
             {
-                count+=mp[sum-k];
+                cnt+=mp[sum-k];
             }
+            // we also add this sum to our map
             mp[sum]++;
         }
-        return count;
+        return cnt;
     }
 };
