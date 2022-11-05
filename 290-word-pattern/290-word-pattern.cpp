f2f69a5c7a81792a5c3dca0baa119cb4cdcdd53c
Daksh Paleria <dpaleria@gmail.com>
2022-11-05 12:33:51 +0530

    Time: 3 ms (50.74%), Space: 6.5 MB (54.09%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        unordered_map<char, int> w2p;
-        unordered_map<string, int> p2w;
-        istringstream in(s); 
+        // so we use a map and everytime for all ele in pattern
+        // we see if its in map or not
+        // if its there we check if its corresponding val is euqals to s
+        // if it isnt we return false, else we return true
+        
+        // but but it says bijection, so we can use two maps. to see if all are bijected
+        // or i can use one map and one set, set will store all chars of s
+        // if a char of s is not in map but its in set means its there in map ie. different pattern with same word which is false
+        vector<string> v;
         string word;
-        int i = 0, n = pattern.size();
-        for(word; in>>word; i++)
+        unordered_map<char, string> mp;
+        unordered_set<string> st;
+        stringstream iss(s);
+        while(iss>>word)
         {
-            if(i==n || w2p[pattern[i]]!=p2w[word])
-            {
-                return false;
-            }
-            w2p[pattern[i]]=p2w[word]=i+1;
+            v.push_back(word);
         }
-        return i==n;
+        if(v.size()!=pattern.size()) return false;
+        for(int i=0; i<pattern.size(); i++)
+        {
+            if(mp.find(pattern[i])!=mp.end())
+            {
+                if(mp[pattern[i]]!=v[i]) return false;
+                else continue;
+            }
+            else
+            {
+                if(st.count(v[i])>0) return false;
+                mp[pattern[i]]=v[i];
+                st.insert(v[i]);
+            }
+        }
+        return true;
     }
 };
