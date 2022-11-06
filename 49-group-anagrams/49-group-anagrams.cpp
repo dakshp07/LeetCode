95b84883f5545f0865ef58dd3c5dbd5aba168e9a
Daksh Paleria <dpaleria@gmail.com>
2022-11-06 11:36:07 +0530

    Time: 68 ms (64.58%), Space: 20.7 MB (39.57%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> umap;
-        vector<vector<string>> ans;
+        // so anagrams when sorted are same
+        // so if we have two words we can sort them up and see if they are really anagrams or not
+        unordered_map<string, vector<string>> mp;
+        // mp will have {sorted words, {all words that are anagrams}}
         for(int i=0; i<strs.size(); i++)
         {
             string temp=strs[i];
-            sort(strs[i].begin(), strs[i].end());
-            umap[strs[i]].push_back(temp); 
+            sort(temp.begin(), temp.end());
+            mp[temp].push_back(strs[i]);
+            // so my map will get {sorted, {anagrams}}
+            // and if we came across any anagram which already has its sorted word as an entry
+            // then map will push it into that specific place
         }
-        for(auto x:umap)
+        vector<vector<string>> res;
+        for(auto it: mp)
         {
-            vector<string> temp=x.second;
-            ans.push_back(temp);
+            res.push_back(it.second);
         }
-        return ans;
+        return res;
     }
 };
