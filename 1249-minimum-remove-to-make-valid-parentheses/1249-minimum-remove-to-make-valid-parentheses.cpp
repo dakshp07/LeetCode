1f9b1d916cda0a7e0498b31f16940ca151482ff7
Daksh Paleria <dpaleria@gmail.com>
2022-11-12 12:57:18 +0530

    Time: 41 ms (68.34%), Space: 11.5 MB (58.37%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
+        // we dont have to do anything to chars
+        // we just need to get rid of wrong paras
+        // so we keep track of index
+        // if we want to exclude any index we keep # at its pos in string
+        // we push all index of open para and pop them one by one as we see a close para
         stack<int> st;
-        int i = 0;
-        for(i=0;i<s.length();i++){
-            if(s[i] == '(' ){
-                st.push(i);
-            }   
-            else if(s[i]==')' && !st.empty()){
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]=='(') st.push(i); // push all open para
+            else if(s[i]==')' && !st.empty())
+            {
+                // we need to pop the ele from stack as it will open bracket
                 st.pop();
             }
-            else if(s[i] == ')' && st.empty()){
-                s[i]='@';
+            // now if we see an close para and my stack is empty then i know
+            // we need to ignore this in our ans
+            else if(s[i]==')' && st.empty())
+            {
+                // mark this index in string
+                s[i]='#';
             }
         }
-        while(!st.empty()){
-            s[st.top()] = '@';
+        // now there still might be some para which are not paired
+        // so we mark them too
+        while(!st.empty())
+        {
+            s[st.top()]='#';
             st.pop();
         }
+        // now we iterate over s and ignore all # indexes
         string res;
-        for(int i=0; i<s.length(); i++)
+        for(int i=0; i<s.size(); i++)
         {
-            if(s[i]!='@')
-            {
-                res.push_back(s[i]);
-            }
+            if(s[i]!='#') res+=s[i];
         }
         return res;
     }
