ec6b19db4530e4b38f7951292d93fed86305d601
Daksh Paleria <dpaleria@gmail.com>
2022-11-16 13:04:46 +0530

    Time: 44 ms (14.84%), Space: 14.2 MB (85.76%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,22 +9,22 @@
  */
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // we will keep on looking for our lca, lca is basically the point where we diverge 
-        // the point where we diverge into left and right of bt is lca
-        
-        // if root is null or if root is equal to p or q then root is our lca
+    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        // base case: if root is null or if root is equal to p or q then root is our lca
         if(root==NULL || root==p || root==q) return root;
-        
-        // if we dont return here then we will recursively call for left side subtree and right subtree
-        TreeNode *lf=lowestCommonAncestor(root->left, p, q);
-        TreeNode *rf=lowestCommonAncestor(root->right, p, q);
-        
-        // if left is null then our ans is right and if right is null our ans is left
-        if(lf==NULL) return rf;
-        if(rf==NULL) return lf;
-        
-        // if none of them are null then root is the point where we diverge
+        // now we go on left, right side
+        // if any of them returns null, that means the nodes are on other side
+        TreeNode* right_node=lca(root->right, p, q);
+        TreeNode* left_node=lca(root->left, p, q);
+        if(right_node==NULL) return left_node;
+        if(left_node==NULL) return right_node;
+        // if in above cases we dont return ans, that means root is our lca
         return root;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // so lca is basically the node where we diverge
+        // we can recursively call for both sides ie left, right
+        return lca(root, p, q);
     }
 };
