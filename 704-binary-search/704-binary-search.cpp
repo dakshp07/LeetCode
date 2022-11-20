8c7dc84e7db7c0fb8b77dd21bd1b51dca0f16c81
Daksh Paleria <dpaleria@gmail.com>
2022-11-20 11:45:07 +0530

    Time: 99 ms (41.52%), Space: 27.7 MB (12.57%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // sada binary search laga denge
-        int low=0;
-        int high=nums.size()-1;
-        while(low<=high)
+        int left=0;
+        int right=nums.size()-1;
+        while(left<=right)
         {
-            int mid=(low+high)/2;
-            if(nums[mid]==target)
-            {
-                return mid;
-            }
-            else if(nums[mid]>target)
-            {
-                high=mid-1;
-            }
-            else
-            {
-                low=mid+1;
-            }
+            int mid=(left+right)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>target) right=mid-1;
+            else left=mid+1;
         }
         return -1;
     }
