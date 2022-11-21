e79c151750cef1e6bb35f9db4b8c691c34796dad
Daksh Paleria <dpaleria@gmail.com>
2022-11-21 12:59:56 +0530

    Time: 92 ms (23.34%), Space: 25.7 MB (94.87%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,12 +1,38 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int length=nums.size();
-        for(int i=0; i<length; i++)
+        // one way is to sort and then sqaure. tc: O(nlogn)
+        // but we need to do in O(n) tc
+        
+        // we will use an extra res vector of same size
+        // and put ele from behind in it through 2 pointers approach
+        vector<int> res(nums.size());
+        // left, right pointer and the curr pointer will be at end of res vector
+        int left=0;
+        int right=nums.size()-1;
+        int curr=nums.size()-1;
+        while(left<=right)
         {
-            nums[i]=nums[i]*nums[i];
+            // if the abs value of ele at low is greater than abs val of ele at high
+            // then we put low at end ie at curr position which starts from end
+            if(abs(nums[left])>abs(nums[right]))
+            {
+                // put it in res
+                res[curr]=nums[left]*nums[left];
+                // decrement curr and increment left
+                curr--;
+                left++;
+            }
+            // in other case our ele at high will have a greater abs value
+            else
+            {
+                // put it in res
+                res[curr]=nums[right]*nums[right];
+                // decrement curr and decrement right
+                curr--;
+                right--;
+            }
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        return res;
     }
 };
