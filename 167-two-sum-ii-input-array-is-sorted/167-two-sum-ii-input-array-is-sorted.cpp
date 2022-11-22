504e4194e67c2ec2d5e7efcf2c4f02ab172fc3da
Daksh Paleria <dpaleria@gmail.com>
2022-11-22 13:26:06 +0530

    Time: 29 ms (53.91%), Space: 15.6 MB (44.41%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+        // in two sum normal question we didnt had array sorted so we used map approach
+        // map approach for two sum normal one: store ele,index and look for target-ele
+        // but here the array is already sorted so we use two pointers here
         int left=0, right=numbers.size()-1;
         while(left<right)
         {
