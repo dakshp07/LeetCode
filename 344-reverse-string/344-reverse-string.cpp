8011571211e600bb838864b28be37942dc959637
Daksh Paleria <dpaleria@gmail.com>
2022-11-23 21:10:31 +0530

    Time: 28 ms (83.13%), Space: 23.1 MB (94.17%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int left=0, right=s.size()-1;
-        while(left<right)
+        // one is to use the inbuilt reverse function
+        // but we use two pointers
+        int left=0;
+        int right=s.size()-1;
+        while(left<=right)
         {
-            swap(s[left], s[right]);
+            swap(s[right], s[left]);
             left++;
             right--;
         }
