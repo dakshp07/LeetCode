4a31265f8aea0651a86e9b2e7f480399498aaca9
Daksh Paleria <dpaleria@gmail.com>
2022-12-13 10:42:39 +0530

    Time: 16 ms (62.36%), Space: 14 MB (79.73%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int sr, int sc, int target_color, int prev_color)
+    void dfs(vector<vector<int>>& image, int sr, int sc, int current_color, int target_color)
     {
-        // base case for dfs
-        if(sr>=image.size() || sc>=image[0].size() || sr<0 || sc<0 || image[sr][sc]!=prev_color) return;
-        // now we make this node as target color
+        // base case
+        // 1. out of bounds
+        // 2. not with color same as previous
+        if(sr<0 || sc<0 || sr>=image.size() || sc>=image[0].size() || image[sr][sc]!=current_color) return;
+        // now we color this ele
         image[sr][sc]=target_color;
-        // and move
-        // up
-        dfs(image, sr-1, sc, target_color, prev_color);
-        // down
-        dfs(image, sr+1, sc, target_color, prev_color);
-        // left
-        dfs(image, sr, sc-1, target_color, prev_color);
-        // right
-        dfs(image, sr, sc+1, target_color, prev_color);
+        // we go up, down, left, right
+        dfs(image, sr-1, sc, current_color, target_color);
+        dfs(image, sr+1, sc, current_color, target_color);
+        dfs(image, sr, sc-1, current_color, target_color);
+        dfs(image, sr, sc+1, current_color, target_color);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        // we will peform a simple dfs on the matrix
-        // we pick up the ele at (sr,sc) and then look for all other neighboring nodes which has same color as the node at (sr, sc)
-        // and mark them with target color
-        
-        // we peform a dfs only if the node is not of target color already
-        int prev_color=image[sr][sc];
-        if(prev_color!=color)
+        // so essentially we need to change color of all the 4 direction elements in grid which have same color as grid[sr][sc]
+        // we can do a dfs or bfs
+        // but i will try dfs here
+        int current_color=image[sr][sc];
+        int target_color=color;
+        if(current_color!=target_color) // we pick node if its current color isnt same as target color
         {
-            dfs(image, sr, sc, color, prev_color);
+            dfs(image, sr, sc, current_color, target_color);
         }
         return image;
     }
