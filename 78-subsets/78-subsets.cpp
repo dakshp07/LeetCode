a0aba1f6ee1d7264d5698e76b006fabb47f82672
Daksh Paleria <dpaleria@gmail.com>
2023-01-06 10:55:48 +0530

    Time: 4 ms (57.35%), Space: 7 MB (95.90%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        if(nums.empty()) return {{}};
-        vector<vector<int>> result={{}};
-        for(int x:nums)
+    void recursion(int ind, vector<int>& nums, vector<int> &ds, vector<vector<int>> &ans)
+    {
+        // base case:
+        // when we reach the end of nums array
+        if(ind==nums.size())
         {
-            int n=result.size();
-            for(int i=0;i<n;i++)
-            {
-               vector<int> r=result[i];
-                r.push_back(x);
-                result.push_back(r);
-            }
+            ans.push_back(ds);
+            return;
         }
-        return result;
+        // if we pick next index
+        ds.push_back(nums[ind]);
+        // now we move to next index
+        recursion(ind+1, nums, ds, ans);
+        ds.pop_back(); // backtrack
+
+        // if we dont pick next one
+        recursion(ind+1, nums, ds, ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // lets use recursion and backtracking
+        vector<int> ds;
+        vector<vector<int>> ans;
+        recursion(0, nums, ds, ans);
+        return ans;
     }
 };
